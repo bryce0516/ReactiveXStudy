525194128564456f5d31a8b3412c71018fd4c989
hyesungpark <jayhspark@gmail.com>
2023-11-14 21:41:16 +0900

    add rxswift image load

--- reactivex/reactivex/ViewController.swift
+++ reactivex/reactivex/ViewController.swift
@@ -6,14 +6,54 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
-
+  
+  var counter: Int = 0
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
+      self.counter += 1
+      self.countLabel.text = "\(self.counter)"
+    }
   }
-
-
+  
+  @IBOutlet var imageView : UIImageView!
+  @IBOutlet var countLabel: UILabel!
+  
+  
+  @IBAction func onLoadImage(_ sender: Any) {
+    imageView.image = nil
+    
+    _ = rxswiftLoadImage(from: LARGE_IMAGE_URL)
+      .observe(on: MainScheduler.instance)
+      .subscribe({ result in
+        switch result {
+        case let .next(image):
+          self.imageView.image = image
+        case let .error(err):
+          print(err.localizedDescription)
+        case .completed:
+          break
+        }
+      })
+    
+  }
+  
+  @IBAction func onCancel(_ sender: Any) {
+    
+  }
+  
+  func rxswiftLoadImage(from imageUrl: String) -> Observable<UIImage?> {
+    return Observable.create { seal in
+      asyncLoadImage(from: imageUrl) { image in
+        seal.onNext(image)
+        seal.onCompleted()
+      }
+      return Disposables.create()
+    }
+  }
 }
 
